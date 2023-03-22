f0b67f1b3fb6aa827ab963f0e04cf2f56f3a1716
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-03-22 13:51:33 +0000

    Append 16 divs to container to create 16x16 grid

---
+++ script.js
@@ -0,0 +1,6 @@
+const gridContainer = document.querySelector("div.gridContainer");
+
+for (let i = 0; i < 16; i++) {
+    const gridDiv = document.createElement("div");
+    gridContainer.appendChild(gridDiv);
+};
