95e2519ca7db360a930f2788a59be068018505f1
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-03-22 21:02:41 +0000

    Fix 16x16 grid and add CSS style

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ let gridSize = 16;
 const gridContainer = document.querySelector(".grid-container");
 
 for (let i = 0; i < gridSize; i++) {
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+    gridContainer.appendChild(gridRow);
     for (let j = 0; j < gridSize; j++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
-        gridContainer.appendChild(gridSquare);
+        gridRow.appendChild(gridSquare);
     }
 };
