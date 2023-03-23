6bf601670974a28eb604a7cf8baf67656c7446fc
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-03-23 12:31:51 +0000

    Add functionality to change grid size

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 let gridSize = 16;
+const gridContainer = document.querySelector(".grid-container");
+createGrid();
 
 // Add event listener to grid size buttons to change grid size
 const gridSizeButtons = document.querySelectorAll(".grid-size");
 gridSizeButtons.forEach((button) => button.addEventListener("click", () => {
     gridSize = button.textContent;
+    createGrid();
 }));
 
-const gridContainer = document.querySelector(".grid-container");
-
-// Create 16x16 grid
-for (let i = 0; i < gridSize; i++) {
-    const gridRow = document.createElement("div");
-    gridRow.classList.add("grid-row");
-    gridContainer.appendChild(gridRow);
-    for (let j = 0; j < gridSize; j++) {
-        const gridSquare = document.createElement("div");
-        gridSquare.classList.add("grid-square");
-        gridRow.appendChild(gridSquare);
+// Create new grid
+function createGrid() {
+    // Remove all grid squares if exists
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
     }
-};
 
-// Add event listener to each grid square
-const gridSquare = document.querySelectorAll(".grid-square");
-gridSquare.forEach((square) => square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "grey";
-}));
+    // Create grid squares
+    for (let i = 0; i < gridSize; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("grid-row");
+        gridContainer.appendChild(gridRow);
+        for (let j = 0; j < gridSize; j++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            gridRow.appendChild(gridSquare);
+        }
+    };
 
+    // Add event listener to each grid square
+    const gridSquare = document.querySelectorAll(".grid-square");
+    gridSquare.forEach((square) => square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "grey";
+    }));
+}
